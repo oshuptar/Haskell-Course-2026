5de5fe4bca978424a2999b6da2d33a2134c79c53
Oleh Shuptar <01193038@pw.edu.pl>
2026-03-18 19:00:58 +0100

    hw1: implementation of task8

--- homeworks/hw1/Solution.hs
+++ homeworks/hw1/Solution.hs
@@ -1,5 +1,6 @@
 module Solution where
 import Data.Function
+{-#LANGUAGE BangPatterns-} -- enables BangPattern extension
 
 -- 1. Write a function `goldbachPairs :: Int -> [(Int, Int)]` that, given an even integer `n ≥ 4`, returns all pairs `(p, q)` satisfying:
 --   - `p` and `q` are both prime numbers
@@ -18,7 +19,7 @@ isPrime n
 
 goldbachPairs :: Int -> [(Int, Int)]
 goldbachPairs n
-    | n < 4 || odd n = [] 
+    | n < 4 || odd n = []
     | otherwise = [(p, q) | q <- [ceiling (fromIntegral n / 2)..n], let p = n - q, isPrime p, isPrime q]
 
 
@@ -30,8 +31,8 @@ removeDuplicates :: Eq a => [a]->[a]
 removeDuplicates xs = reverse (go xs [])
     where
         go [] acc = acc
-        go (x:xs) !acc = if 
-            all (\y -> y /= x) xs 
+        go (x:xs) !acc = if
+            all (\y -> y /= x) xs
             then go xs (x : acc)
             else go xs acc
 -- since appending to the end of the list is O(n) on each step. We reverse it once at the end with complexity O(n)
@@ -42,10 +43,10 @@ coprimePairs :: [Int] -> [(Int, Int)]
 coprimePairs [] = []
 coprimePairs xs
     | any ( <= 0) xs = []
-    | otherwise = go (removeDuplicates xs) 
+    | otherwise = go (removeDuplicates xs)
  where
     go [] = []
-    go (x:xs) = [(min x y, max x y) | y <- xs , x /= y ,x `gcd` y == 1] ++ go xs 
+    go (x:xs) = [(min x y, max x y) | y <- xs , x /= y ,x `gcd` y == 1] ++ go xs
     -- we handle the case x /= y, because if x = 1 and y = 1 => gcd x y == 1, but x is not < y
 
 -- 3. Sieve of Eratosthenes
@@ -54,7 +55,7 @@ sieve [] = []
 sieve (x:xs) = x : sieve [y | y <- xs, y `mod` x/= 0]
 
 primesTo :: Int -> [Int]
-primesTo n 
+primesTo n
     | n < 2 = []
     | otherwise = sieve [2..n]
 
@@ -74,7 +75,7 @@ matMul a b = [
     [ sum [ a !! i !! k * b !! k !! j | k <- [0..p-1]]
     | j <- [0..n-1]]
     | i <- [0..m-1]]
-    where 
+    where
         m = length a
         p = length (head a)
         n = length (head b)
@@ -83,7 +84,7 @@ matMul a b = [
 permutations :: Int -> [a] -> [[a]]
 permutations 0 set = [[]];
 permutations _ []  = []
-permutations k (x:xs) 
+permutations k (x:xs)
     | k == length xs + 1 = [ x:tail | tail <- (k-1) `permutations` xs ] -- ensures that every subset is k-element
     | otherwise = [ x:tail | tail <- (k-1) `permutations` xs ] ++ k `permutations` xs
 
@@ -93,17 +94,34 @@ permutations k (x:xs)
 merge :: Ord a => [a] -> [a] -> [a]
 merge a [] = a
 merge [] b = b
-merge (x:xs) (y:ys) 
+merge (x:xs) (y:ys)
     | x == y = x : merge xs ys -- reccursively calls the merge on the tail of the list
     | x > y = y : merge (x:xs) ys
-    | otherwise = x:merge xs (y:ys) 
+    | otherwise = x:merge xs (y:ys)
 
 hamming :: [Integer]
 hamming = 1 : merge (merge (map (2*) hamming) (map (3*) hamming)) (map (5*) hamming)
 
 -- 7. Integer Power with Bang Patterns
 power::Int -> Int -> Int
-power b e = go e 1
+power b 0 = 1
+power !b e = b * power b (e-1)
+
+-- 8. Running Maximum: `seq` vs. Bang Patterns
+-- forces evaluation before passing to the function call
+listMax :: [Int] -> Maybe Int
+listMax [] = Nothing
+listMax (x:xs) = go xs x
+    where
+        go [] acc = Just acc
+        go (y:ys) acc =
+            let newAcc = max y acc
+            in seq newAcc (go ys newAcc)
+
+-- forces evaluation on binding
+listMax' :: [Int] -> Maybe Int
+listMax'[] = Nothing
+listMax' (x:xs) = go xs x
     where 
-        go 0 !acc = acc
-        go e !acc = go (e-1) b*acc
+        go [] !acc = Just acc
+        go (y:ys) !acc = go ys (max y acc)
