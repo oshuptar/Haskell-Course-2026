01ce546941da483227a095d55a88668653e26b94
Oleh Shuptar <01193038@pw.edu.pl>
2026-03-25 20:07:16 +0100

    hw2: implementation of task2

--- homeworks/hw2/Solution.hs
+++ homeworks/hw2/Solution.hs
@@ -9,7 +9,14 @@ instance Functor Sequence where
     fmap f (Single a) = Single(f a)
     fmap f (Append a1 a2) = Append (fmap f a1) (fmap f a2)
 
+instance Foldable Sequence where
+    -- foldmap :: Monoid m => (a -> m) -> Sequence a -> m
+    foldMap f (Empty) = mempty
+    foldMap f (Single a) = f a
+    foldMap f (Append a1 a2) = foldMap f a1 <> foldMap f a2
+
 main :: IO ()
 main = do
     let sequence = Append (Single 1) (Append (Single 2) (Append (Single 3) Empty))
     print(fmap (+1) sequence)
+    print(foldMap (\a -> [a]) sequence)
