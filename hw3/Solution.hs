925ff82e04124513dfb441a6d3b2e975eb7b4e84
Oleh Shuptar <01193038@pw.edu.pl>
2026-04-22 22:08:06 +0200

    hw3: implementation of task3

--- hw3/Solution.hs
+++ hw3/Solution.hs
@@ -1,5 +1,7 @@
 module Solution (main) where
 import qualified Data.Map as Map
+import Data.List (permutations)
+import Control.Monad (guard)
 
 type Pos = (Int, Int)
 data Dir = N | S | E | W deriving (Eq, Ord, Show)
@@ -37,6 +39,40 @@ decryptWords key (word:sentence) = do
     decryptedSentence <- decryptWords key sentence
     return ([decryptedWord] <> decryptedSentence)
 
+-- 3.
+type Guest = String
+type Conflict = (Guest, Guest)
+
+select :: [a] -> [(a, [a])]
+select [] = []
+select (x:xs) =
+    (x, xs) : [(y, x:ys) | (y, ys) <- select xs]
+
+safePair :: (Guest, Guest) -> [Conflict] -> Bool
+safePair (guest, adjacentGuest) = all (\conflict -> (guest, adjacentGuest) /= conflict && (adjacentGuest, guest) /= conflict)
+
+seatings' :: [Guest] -> [Conflict] -> [[Guest]]
+seatings' [] _ = [[]]
+seatings' guests conflicts = do
+    (guest, rest) <- select guests
+    arrangement <- seatings' rest conflicts -- at this point we assume all conflicts before are filtered out
+    case arrangement of
+        [] -> guard True
+        adjacentGuest:_ -> guard (safePair (guest, adjacentGuest) conflicts)
+    return (guest:arrangement)
+
+seatings :: [Guest] -> [Conflict] -> [[Guest]]
+seatings guests conflicts = do
+    arrangement <- seatings' guests conflicts
+    case arrangement of
+        [] -> return arrangement
+        [_] -> return arrangement
+        _ -> do
+            let firstGuest = head arrangement
+                lastGuest  = last arrangement
+            guard (safePair (firstGuest, lastGuest) conflicts)
+            return arrangement
+
 
 main :: IO ()
 main = do
@@ -69,5 +105,15 @@ main = do
     print $ decrypt key "abcdexyz"
     print $ decryptWords key ["abc", "bcd", "xyz"]
     print $ decryptWords key ["abc", "bcde", "xyz"]
+
+    putStrLn "\nTask3"
+    let guests :: [Guest] = ["Lannister", "Targaryen", "Stark"]
+    let conflicts :: [Conflict] = [ ("Lannister", "Stark")]
+    let guests2 :: [Guest] = ["Lannister", "Targaryen", "Stark", "Baratheon"]
+    let conflicts2 :: [Conflict] =[ ("Lannister", "Stark"), ("Targaryen", "Baratheon"), ("Lannister", "Targaryen")]
+    print $ seatings guests []
+    print $ seatings guests conflicts
+    print $ seatings guests2 conflicts2
+    print $ length (seatings guests2 [])
 
 
