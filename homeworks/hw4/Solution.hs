614c3fb685699fb4374c4525dfbdb1f791f722ba
Oleh Shuptar <01193038@pw.edu.pl>
2026-05-06 17:37:01 +0200

    hw4: implementation of part3

--- homeworks/hw4/Solution.hs
+++ homeworks/hw4/Solution.hs
@@ -24,14 +24,54 @@ ask :: Reader r r
 ask = Reader id
 
 asks :: (r -> a) -> Reader r a
-asks f = Reader (\env -> f env)
+-- asks f = Reader (\env -> f env)
+asks f = do f <$> ask
 
 local :: (r -> r) -> Reader r a -> Reader r a
-local envModifier reader = Reader (\env -> runReader reader (envModifier env))
+-- local envModifier reader = Reader (\env -> runReader reader (envModifier env))
+local envModifier reader = do
+    env <- ask
+    let localEnv = envModifier env
+    return (runReader reader localEnv)
+
 
 -- 3:
+data BankConfig = BankConfig {
+    interestRate :: Double,
+    transactionFee :: Int,
+    minimumBalance :: Int
+} deriving (Show)
 
+data Account = Account {
+    accountId :: String,
+    balance :: Int
+} deriving (Show)
+
+calculateInterest :: Account -> Reader BankConfig Int
+calculateInterest account = do
+    rate <- asks interestRate
+    let accountBalance = balance account
+    return (round (rate * fromIntegral accountBalance))
+
+applyTransactionFee :: Account -> Reader BankConfig Account
+applyTransactionFee account = do
+    fee <- asks transactionFee
+    return (Account (accountId account) (balance account - fee))
+
+checkMinimumBalance :: Account -> Reader BankConfig Bool
+checkMinimumBalance account = do
+    threshold <- asks minimumBalance
+    return (balance account >= threshold)
+
+processAccount :: Account -> Reader BankConfig (Account, Int, Bool)
+processAccount account = do
+    updated <- applyTransactionFee account
+    interest <- calculateInterest account
+    minBalanceFlag <- checkMinimumBalance account
+    return (updated, interest, minBalanceFlag)
 
 main :: IO ()
 main = do
-    undefined
+    let cfg = BankConfig { interestRate = 0.05, transactionFee = 2, minimumBalance = 999 }
+    let acc = Account { accountId = "A-001", balance = 1000 }
+    print $ runReader (processAccount acc) cfg
