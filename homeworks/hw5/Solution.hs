13216d8840cd7932737d1744939f389b1ffbae50
Oleh Shuptar <01193038@pw.edu.pl>
2026-05-06 19:08:00 +0200

    hw5: implementation of part2

--- homeworks/hw5/Solution.hs
+++ homeworks/hw5/Solution.hs
@@ -1,5 +1,6 @@
 module Solution(main) where
 import Control.Monad.State
+import Data.Map
 
 data Instr = PUSH Int | POP | DUP | SWAP | ADD | MUL | NEG
 
@@ -45,16 +46,58 @@ execProg (head:remaining) = do
 runProg :: [Instr] -> [Int]
 runProg instructions = let (res, state) = runState (execProg instructions) [] in state
 
+-- 2:
+data Expr = Num Int | Var String | Add Expr Expr | Mul Expr Expr | Neg Expr | Assign String Expr | Seq Expr Expr
+eval :: Expr -> State (Map String Int) Int
+eval (Num !val) = return val
+eval (Var variable) = do
+    map <- get
+    case Data.Map.lookup variable map of
+        Just val -> return val
+eval (Add !exprL !exprR) = do
+    resultL <- eval exprL
+    resultR <- eval exprR
+    return (resultL + resultR)
+eval (Mul !exprL !exprR) = do
+    resultL <- eval exprL
+    resultR <- eval exprR
+    return (resultL * resultR)
+eval (Neg !expr) = do
+    result <- eval expr
+    return (-result)
+eval (Assign var expr) = do
+    result <- eval expr
+    modify (Data.Map.insert var result)
+    return result
+eval (Seq exprL exprR) = do
+    eval exprL
+    eval exprR
+
+runEval :: Expr -> Int
+runEval expression = let (res,state) = runState (eval expression) Data.Map.empty in res
+
 
 main :: IO ()
 main = do
+    putStrLn "Part1"
     let testProgram = [PUSH 3, PUSH 4, ADD, DUP, PUSH 2, MUL, SWAP, NEG, POP]
     let expected = [14]
     print $ runProg testProgram
     print $ runProg testProgram == expected
 
     let skipProgram = [POP, DUP, SWAP, ADD, MUL, NEG, PUSH 10, SWAP, ADD, MUL, PUSH 5, SWAP]
-    let skipExpected = [10,5]
+    let expected = [10,5]
     print $ runProg skipProgram
-    print $ runProg skipProgram == skipExpected
+    print $ runProg skipProgram == expected
+
+    putStrLn "\nPart2"
+    let testEval = Seq (Assign "x" (Num 10)) (Seq (Assign "y" (Add (Var "x") (Num 5))) (Seq (Assign "z" (Mul (Var "y") (Neg (Num 2)))) (Add (Var "z") (Var "x"))))
+    let expected = -20
+    print $ runEval testEval
+    print $ runEval testEval == expected
+
+    let testEval = Seq (Assign "a" (Num 4)) (Seq (Assign "b" (Mul (Var "a") (Num 3))) (Seq (Assign "a" (Add (Var "b") (Neg (Var "a")))) (Mul (Var "a") (Add (Var "b") (Num 1)))))
+    let expected = 104
+    print $ runEval testEval
+    print $ runEval testEval == expected
 
