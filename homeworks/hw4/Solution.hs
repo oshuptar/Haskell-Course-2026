572837eb20e2353bee384159a20518a8122f3636
Oleh Shuptar <01193038@pw.edu.pl>
2026-05-06 16:04:21 +0200

    hw4: implementation of part2

--- homeworks/hw4/Solution.hs
+++ homeworks/hw4/Solution.hs
@@ -13,11 +13,23 @@ instance Applicative (Reader r) where
     -- pure :: a -> Reader r a
     pure val = Reader (const val)
     -- liftA2 :: (a -> b -> c) -> Reader r a -> Reader r b -> Reader r c
-    liftA2 f reader1 reader2 = Reader(\env ->  f (runReader reader1 env) (runReader reader2 env))
+    liftA2 f reader1 reader2 = Reader (\env ->  f (runReader reader1 env) (runReader reader2 env))
 
 instance Monad (Reader r) where
     -- >>= :: Reader r a -> (a -> Reader r b) -> Reader r b
     reader >>= f =  Reader (\env -> runReader (f (runReader reader env)) env)
+
+-- 2:
+ask :: Reader r r
+ask = Reader id
+
+asks :: (r -> a) -> Reader r a
+asks f = Reader (\env -> f env)
+
+local :: (r -> r) -> Reader r a -> Reader r a
+local envModifier reader = Reader (\env -> runReader reader (envModifier env))
+
+-- 3:
 
 
 main :: IO ()
