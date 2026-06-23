8f62fbc1cfbeb35990801d34edec7feb17602f1f
Oleh Shuptar <01193038@pw.edu.pl>
2026-06-23 13:09:33 +0200

    feat(project): implementation of the contract of building

--- project/src/BuildSysLang/DependencyResolver.hs
+++ project/src/BuildSysLang/DependencyResolver.hs
@@ -1,9 +1,12 @@
-module BuildSysLang.DependencyResolver where
+module BuildSysLang.DependencyResolver (build, buildDependencyGraph, topologicalSort) where
 import qualified Data.Map as Map
 import Data.Map (Map)
 import BuildSysLang.AST (Target, Rule (..), BuildFile (..), Command (..))
-import Control.Monad.State (StateT, get, modify, runStateT, evalStateT)
+import Control.Monad.State (StateT, get, modify, runStateT)
 import Control.Monad.Trans.Class (lift)
+import qualified Data.Set as Set
+import qualified Control.Monad
+import Data.Set (Set)
 
 data DependencyGraph = DependencyGraph {
     adjacencyGraph :: Map Target [Target],
@@ -26,15 +29,6 @@ buildDependencyGraph (BuildFile rules) = DependencyGraph {
     dependencyRule = Map.fromList (buildDependencyRule rules)
 }
 
-extractDependencies :: [Rule] -> [Target]
-extractDependencies rules = rules >>= dependencies
-
-buildDependencyList :: [Rule] -> [(Target, [Target])]
-buildDependencyList = fmap (\rule -> (target rule, dependencies rule))
-
-buildDependencyRule :: [Rule] -> [(Target, Rule)]
-buildDependencyRule = fmap (\ rule -> (target rule, rule))
-
 topologicalSort :: DependencyGraph -> Either Cycle [Target]
 topologicalSort graph = let initialState = VisitState {
     colors   = Map.map (const White) (adjacencyGraph graph),
@@ -44,6 +38,15 @@ topologicalSort graph = let initialState = VisitState {
         Left cycle -> Left cycle
         Right (_, finalState) -> Right (reverse (ordering finalState))
 -- runStateT action s0 :: Either Cycle ((), VisitState)
+
+extractDependencies :: [Rule] -> [Target]
+extractDependencies rules = rules >>= dependencies
+
+buildDependencyList :: [Rule] -> [(Target, [Target])]
+buildDependencyList = fmap (\rule -> (target rule, dependencies rule))
+
+buildDependencyRule :: [Rule] -> [(Target, Rule)]
+buildDependencyRule = fmap (\ rule -> (target rule, rule))
 
 markVisited :: Target -> StateT VisitState (Either Cycle) ()
 markVisited target = do
@@ -60,9 +63,6 @@ markFinished target = do
         path = tail (path visitState)
     })
 
-buildCyclePath :: Target -> [Target] -> [Target]
-buildCyclePath target path = reverse (takeWhile (/= target) path) ++ [target]
-
 visitNode :: DependencyGraph -> Target -> StateT VisitState (Either Cycle) ()
 visitNode graph target = do
     visitState <- get
@@ -77,7 +77,49 @@ visitNode graph target = do
         (Just Black) -> return ()
         _ -> error "All targets must be present"
 
+buildCyclePath :: Target -> [Target] -> [Target]
+buildCyclePath target path = reverse (takeWhile (/= target) path) ++ [target]
 
+type FreshnessCheck = Target -> IO Bool -- a function which would determine whether a Target needs rebuilding
 
+build :: DependencyGraph -> FreshnessCheck -> IO ()
+build graph freshnessCheck =
+  case topologicalSort graph of
+    Left cycle  -> putStrLn $ "Build failed: Circular dependency " ++ show cycle -- report the cycle, abort
+    Right topoOrder -> do
+      (_, rebuildSet) <- runStateT (refreshGraph graph topoOrder freshnessCheck) Set.empty
+      success <- rebuildGraph graph rebuildSet topoOrder
+      unless success $ putStrLn "Build failed: a recipe errored"
 
+-- a computation which tracks the set of targets that needs rebuilding
+refreshGraph :: DependencyGraph -> [Target] -> FreshnessCheck -> StateT (Set Target) IO ()
+refreshGraph graph [] freshnessCheck = return ()
+refreshGraph graph (dependency:targets) freshnessCheck = do
+    rebuildState <- get
+    needsRebuilding <- lift (freshnessCheck dependency)
+    case Map.lookup dependency (adjacencyGraph graph) of
+        Just dependencies -> Control.Monad.when (any (`Set.member` rebuildState) dependencies || needsRebuilding) $ modify (Set.insert dependency)
+        Nothing -> return ()
+    refreshGraph graph targets freshnessCheck
 
+rebuildGraph :: DependencyGraph -> Set Target -> [Target] -> IO Bool
+rebuildGraph graph rebuildSet [] = return True
+rebuildGraph graph rebuildSet (target:rest) = do
+    result <- if Set.member target rebuildSet
+              then case Map.lookup target (dependencyRule graph) of
+                       Just rule -> executeRecipe (recipe rule)
+                       Nothing -> return True
+              else return True
+    if result then rebuildGraph graph rebuildSet rest else return False
+
+executeCommand :: Command -> IO Bool
+executeCommand command = do
+    undefined
+    -- parse and execute the command here
+
+-- Executes a recipe in the terminal
+executeRecipe :: [Command] -> IO Bool
+executeRecipe [] = return True
+executeRecipe (command:rest) = do
+    result <- executeCommand command
+    if result then executeRecipe rest else return False
